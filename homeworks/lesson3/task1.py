677da639a6761d70aba0ad45a30a28b20854bb1a
nat0t <nataniel1987@gmail.com>
2020-06-17 17:14:41 +0300

    added task1

---
+++ homeworks/lesson3/task1.py
@@ -0,0 +1,18 @@
+'''
+Реализовать функцию, принимающую два числа (позиционные аргументы) и
+выполняющую их деление. Числа запрашивать у пользователя,
+предусмотреть обработку ситуации деления на ноль.
+'''
+def my_division(x, y) -> float:
+    try:
+        return float(x) / float(y)
+    except ValueError:
+        print("It's not numbers.")
+    except ZeroDivisionError:
+        print('Cannot be divided by zero.')
+    else:
+        print('Unknown error.')
+
+x = input('Enter dividend (x): ')
+y = input('Enter divider (y): ')
+print(f'x / y = {my_division(x, y)}')
