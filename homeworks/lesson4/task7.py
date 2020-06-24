4683d5dab52721a1b1d7ca5c5b06fa8db5dc3b8a
nat0t <nataniel1987@gmail.com>
2020-06-24 17:17:59 +0300

    modified task7.py

--- homeworks/lesson4/task7.py
+++ homeworks/lesson4/task7.py
@@ -1,13 +1,13 @@
 '''Реализовать генератор с помощью функции с ключевым словом yield,
 создающим очередное значение. При вызове функции должен создаваться
-объект-генератор. Функция должна вызываться следующим образом: for el in fact(n).
-Функция отвечает за получение факториала числа, а в цикле необходимо выводить
-только первые n чисел, начиная с 1! и до n!.
-Подсказка: факториал числа n — произведение чисел от 1 до n. Например,
-факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
+объект-генератор. Функция должна вызываться следующим образом: for
+el in fact(n). Функция отвечает за получение факториала числа, а
+в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
+Подсказка: факториал числа n — произведение чисел от 1 до n.
+Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 '''
 
-def fact(n: int) -> int:
+def fact1(n: int) -> int:
     """
     Function takes parameter 'n' and returns 'n!'.
     :param n:
@@ -15,12 +15,22 @@ def fact(n: int) -> int:
     """
     import math
     number = 1
-    if n == 0:
-        yield 0, 1
-    else:
-        while number <= n:
-            yield number, math.factorial(number)
-            number += 1
+    while number <= n:
+        yield number, math.factorial(number)
+        number += 1
+
+def fact2(n: int) -> int:
+    """
+    Function takes parameter 'n' and returns 'n!'.
+    :param n:
+    :return:
+    """
+    start = 1
+    result = 1
+    while n >= start:
+        yield result
+        start += 1
+        result *= start
 
 while True:
     try:
@@ -29,5 +39,9 @@ while True:
     except ValueError:
         print('Enter natural number!')
 
-for number, fact_ in fact(n):
+print('Variant 1:')
+for number, fact_ in fact1(n):
     print(f'{number}! = {fact_}')
+print('Variant 2:')
+for index, number in enumerate(fact2(n), 1):
+    print(f'{index}! = {number}')
